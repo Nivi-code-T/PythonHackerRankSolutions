0d9f71fe95cea6753c3a65e407453b30f297a2f3
Nivi-code-T <niveteggi@gmail.com>
2026-08-06 15:18:08 -0500

    printfullname

---
+++ PrintFullName.py
@@ -0,0 +1,6 @@
+def printfullname(first,last):
+    print(f"Hello {first_name} {last_name} ! You just delved into python.")
+
+first_name=input()
+last_name=input()
+printfullname(first_name,last_name)
