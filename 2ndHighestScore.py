008a07b43654610ce8c8af903ce1af8436aa7b32
Nivi-code-T <niveteggi@gmail.com>
2026-09-07 16:01:38 -0500

    Second Highest Score

--- 2ndHighestScore.py
+++ 2ndHighestScore.py
@@ -1,6 +1,6 @@
 def secondhighestScorefromlist(scores):
 
-    scores=list(set(scores))  # it will remove the duplicates in the list
+    scores = list(set(scores))  # it will remove the duplicates in the list
     scores.sort()
     #print(scores)
     return scores[-2]
