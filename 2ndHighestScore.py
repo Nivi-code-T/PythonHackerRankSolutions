dff205099a889914930c5bfa649810644f87fa6d
Nivi-code-T <niveteggi@gmail.com>
2026-09-07 15:58:43 -0500

    NestedLists

--- 2ndHighestScore.py
+++ 2ndHighestScore.py
@@ -2,12 +2,12 @@ def secondhighestScorefromlist(scores):
 
     scores=list(set(scores))  # it will remove the duplicates in the list
     scores.sort()
-    print(scores)
+    #print(scores)
     return scores[-2]
 
 n=int(input())
 scores = list(map(int, input().split()))
-print(scores)
+#print(scores)
 print(secondhighestScorefromlist(scores))
 
 
